7d7811d3e37a1c1e0f2a53350415293ada4d27c6
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2017-08-01 22:08:48 +0530

    Working Example

--- spring-module/src/main/java/org/spring/module/SpringModuleApplication.java
+++ spring-module/src/main/java/org/spring/module/SpringModuleApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -27,7 +26,6 @@ import org.springframework.web.servlet.DispatcherServlet;
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
 public class SpringModuleApplication extends SpringBootServletInitializer {
 
 	/**
